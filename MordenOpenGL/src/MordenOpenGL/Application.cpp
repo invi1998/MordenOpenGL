79ac45319f3d4b070f710ebcbf9a70dc804d2b10
invi1998 <2267661826@qq.com>
2023-05-08 17:22:20 +0800

    初始化GLAD

--- MordenOpenGL/src/MordenOpenGL/Application.cpp
+++ MordenOpenGL/src/MordenOpenGL/Application.cpp
@@ -24,15 +24,16 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // GLAD是用来管理OpenGL的函数指针的，所以在调用任何OpenGL的函数之前我们需要初始化GLAD。
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        // GLAD是用来管理OpenGL的函数指针的，所以在调用任何OpenGL的函数之前我们需要初始化GLAD。
-        if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-        {
-            std::cout << "Failed to initialize GLAD" << std::endl;
-            continue;
-        }
 
         /* Render here */
         glClearColor(0.1f, 0.1f, 0.23f, 1.0f);
