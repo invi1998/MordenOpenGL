e27a402bf173c848af1d4879ddf3a1d2d7d78320
invi1998 <2267661826@qq.com>
2023-05-08 18:12:52 +0800

    窗口

--- MordenOpenGL/src/MordenOpenGL/Application.cpp
+++ MordenOpenGL/src/MordenOpenGL/Application.cpp
@@ -5,47 +5,76 @@
 #include <GLFW/glfw3.h>
 
 
+// 窗口大小调整时的回调函数
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// 退出按钮事件
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+
 int main(void)
 {
-    GLFWwindow *window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	// glfw 初始化glfw和配置OpenGL
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// MAC 下需要添加下面这行代码，配置才能生效
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	// 创建一个窗口对象，这个窗口对象存放了所有和窗口相关的数据，而且会被GLFW的其他函数频繁地用到
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	//创建完窗口我们就可以通知GLFW将我们窗口的上下文设置为当前线程的主上下文了。
+	glfwMakeContextCurrent(window);
 
-    // GLAD是用来管理OpenGL的函数指针的，所以在调用任何OpenGL的函数之前我们需要初始化GLAD。
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	// 初始化GLAD，传入用来加载系统相关的OpenGL函数指针地址的函数
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+	// 设置视口
+	glViewport(0, 0, 800, 600);
 
-        /* Render here */
-        glClearColor(0.1f, 0.1f, 0.23f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+	// 注册窗口resize时的回调函数
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	// 渲染循环
+	while(!glfwWindowShouldClose(window))
+	{
+		// 事件输入
+		processInput(window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		// 渲染指令
+		glClearColor(0.1f, 0.1f, 0.215f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-    glfwTerminate();
-    return 0;
+		// 检查并调用事件，交换缓冲
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+
 }
