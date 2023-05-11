d7cee483c8fc588eef26b80b0c9508336d057a06
invi1998 <2267661826@qq.com>
2023-05-11 17:53:20 +0800

    新增glm

--- Dependencies.lua
+++ Dependencies.lua
@@ -5,6 +5,7 @@ IncludeDir = {}
 IncludeDir["stb_image"] = "%{wks.location}/MordenOpenGL/vendor/stb_image"
 IncludeDir["GLFW"] = "%{wks.location}/MordenOpenGL/vendor/GLFW/include"
 IncludeDir["Glad"] = "%{wks.location}/MordenOpenGL/vendor/Glad/include"
+IncludeDir["glm"] = "%{wks.location}/MordenOpenGL/vendor/glm"
 
 LibraryDir = {}
 
