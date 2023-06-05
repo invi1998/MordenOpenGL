a8c691fe8f4e526d95602c461ec462fa37d488f6
invi_1998 <2267661826@qq.com>
2023-06-05 17:24:34 +0800

    锐化效果

--- MordenOpenGL/asserts/shaders/screen.glsl
+++ MordenOpenGL/asserts/shaders/screen.glsl
@@ -24,6 +24,8 @@ in vec2 texCoord;
 
 uniform sampler2D u_Texture;
 
+const float offset = 1.0 / 300.0;
+
 void main()
 {
 	// vec3 col = texture(u_Texture, texCoord).rgb;
@@ -32,11 +34,43 @@ void main()
 	// 反相 从屏幕纹理中取颜色值，然后用1.0减去它，对它进行反相：
 	// FragColor = vec4(vec3(1.0 - texture(u_Texture, texCoord)), 1.0);
 
-	// 灰度 取所有的颜色分量，将它们平均化：
-	FragColor = texture(u_Texture, texCoord);
-	// float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
-	// 这已经能创造很好的结果了，但人眼会对绿色更加敏感一些，而对蓝色不那么敏感，所以为了获取物理上更精确的效果，我们需要使用加权的(Weighted)通道：
-	float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
-	FragColor = vec4(average, average, average, 1.0);
+//	// 灰度 取所有的颜色分量，将它们平均化：
+//	FragColor = texture(u_Texture, texCoord);
+//	// float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+//	// 这已经能创造很好的结果了，但人眼会对绿色更加敏感一些，而对蓝色不那么敏感，所以为了获取物理上更精确的效果，我们需要使用加权的(Weighted)通道：
+//	float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+//	FragColor = vec4(average, average, average, 1.0);
+
+	// 核 （卷积）
+	vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // 左上
+        vec2( 0.0f,    offset), // 正上
+        vec2( offset,  offset), // 右上
+        vec2(-offset,  0.0f),   // 左
+        vec2( 0.0f,    0.0f),   // 中
+        vec2( offset,  0.0f),   // 右
+        vec2(-offset, -offset), // 左下
+        vec2( 0.0f,   -offset), // 正下
+        vec2( offset, -offset)  // 右下
+    );
+
+    float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );
+
+    vec3 sampleTex[9];
+    for (int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(u_Texture, texCoord.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for (int i = 0; i < 9; i++)
+    {
+        col += sampleTex[i] * kernel[i];
+    }
+
+    FragColor = vec4(col, 1.0);
 
 }
