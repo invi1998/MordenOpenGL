03af2b13eda06c041f9674a05fd226ffa73f3c88
invi_1998 <2267661826@qq.com>
2023-06-08 18:13:34 +0800

    几何着色器绘制房子

--- MordenOpenGL/asserts/shaders/passThrough.glsl
+++ MordenOpenGL/asserts/shaders/passThrough.glsl
@@ -13,13 +13,26 @@ void main()
 #version 330 core
 
 layout (points) in;
-layout (points, max_vertices = 1) out;
+layout (triangle_strip, max_vertices = 5) out;
+
+void build_house(vec4 position)
+{    
+    gl_Position = position + vec4(-0.2, -0.2, 0.0, 0.0);    // 1:左下
+    EmitVertex();   
+    gl_Position = position + vec4( 0.2, -0.2, 0.0, 0.0);    // 2:右下
+    EmitVertex();
+    gl_Position = position + vec4(-0.2,  0.2, 0.0, 0.0);    // 3:左上
+    EmitVertex();
+    gl_Position = position + vec4( 0.2,  0.2, 0.0, 0.0);    // 4:右上
+    EmitVertex();
+    gl_Position = position + vec4( 0.0,  0.4, 0.0, 0.0);    // 5:顶部
+    EmitVertex();
+    EndPrimitive();
+}
 
 void main()
 {
-	gl_Position = gl_in[0].gl_Position;
-	EmitVertex();
-	EndPrimitive();
+	build_house(gl_in[0].gl_Position);
 }
 
 
